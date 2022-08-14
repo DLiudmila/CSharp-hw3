77ee197ff31ac2a8621cad34eaecd9a9dd842e05
Liudmila D <verliebtin@mail.ru>
2022-08-14 21:25:51 +0300

    hw

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.Write(n+" ->");
+while (i<=n)
+{
+    Console.Write(" "+ Math.Pow(i, 3));
+    i++;
+}
+Console.WriteLine();
